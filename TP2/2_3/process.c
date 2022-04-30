7214a5656d33312b0d49ed5b421094737d1f8b5c
Alex Fernandez <javier.fddz@gmail.com>
2022-04-30 11:45:26 +0200

    Clean and Comment Pending Fns

--- TP2/2_3/process.c
+++ TP2/2_3/process.c
@@ -5,6 +5,8 @@
 // Declaring the enum argument for chosen letter case
 enum casetype {lower_case, upper_case};
 
+/* Transform each char in a char array to 
+   upper or lower case (or not) */
 char* transformString(char* string, enum casetype sel_case) {
     int ch = 0; // var to track character in string
 
@@ -30,12 +32,14 @@ char* transformString(char* string, enum casetype sel_case) {
 
 int main() {
     // Initialize program variables
-    char *str; // given string
-    char in_char; // incoming character
-    int str_len = 0; // length of given string
+    char *str; // given string (char array)
+    char in_char; // incoming character (input)
+    unsigned int str_len = 0; // length of given string
     int input_case_opt; // option input for case transformation 
     enum casetype selected_case; // variable of type casetype
-    // int word_count = 1;
+    char **text; // table of text (array of char arrays)
+    unsigned int word_count = 0; // stats: word count
+    unsigned float avg_letters = 0; // stats: avg letters per word
 
     // Allocate memory for at least 1 character 
     str = (char*)malloc(sizeof(char) * 1);
@@ -60,19 +64,18 @@ int main() {
     scanf(" %d", &input_case_opt);
     selected_case = input_case_opt; // assign input to enum var 
 
+    // Transform string to Upper or Lower case
     str = transformString(str, selected_case);
     printf("Here are the words that you gave after changing the case:\n");
     printf("%s", str);
+
+    /* Get string stats (word count and avg chars per word) */
+    // Process the string and build the text table
 
 
     printf("\n\n");
     // Free dynamically allocated memory
     free(str);
-
-    // for (int word = 0; word < word_count; word++) {
-    //     free(text[word]);
-    // }
-    // free(text);
 
     return 1;
 }
