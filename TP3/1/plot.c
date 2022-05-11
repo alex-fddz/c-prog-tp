8527789098fb7c85fbd55f9412c348579a62ac51
Javier Alejandro FERNANDEZ CORDOVA <j22ferna@pc-b33-06.rennes.enst-bretagne.fr>
2022-05-11 15:06:38 +0200

    Gather params for plot

--- TP3/1/plot.c
+++ TP3/1/plot.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #ifdef __unix__
 #define drawPlot(f) \
-    snprintf(cmd, sizeof(cmd), "gnuplot --persist -e 'plot \"%s\"'", f); \
-    system(cmd);
+    system("gnuplot --persist -e 'plot \"function_data\"'");
 #else
 #error "OS not supported!"    
 #endif
 
-int main() {
-    char cmd[100];
-    char* filename = "function_data";
-    
-    drawPlot(filename);
+int main(int argc, char** argv) {
+    // Variable initialization
+    double mean, std, n; // Input params
+    double low_bound, upp_bound, step_size; // Calc params 
+    // Check for complete parameter input
+    if (argc < 4) {
+        printf("Please provide the complete parameters.\n");
+        return 0;
+    }
 
+    // Gather params
+    mean = atof(argv[1]);
+    std = atof(argv[2]);
+    n = atof(argv[3]);
+
+    // Check params are valid
+    if (n==0) return -1;
+
+    // Generate normal distribution 
+    low_bound = -3 * std;
+    upp_bound = 3 * std;
+    step_size = (upp_bound - low_bound) / n;
+
+    drawPlot(filename);
     
     return 1;
 }
