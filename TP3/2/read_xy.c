e8c1779808f5d693dabb3db5f4a41f4aaa18807d
Alex Fernandez <javier.fddz@gmail.com>
2022-05-14 10:25:18 +0200

    Save Numbers in array

--- TP3/2/read_xy.c
+++ TP3/2/read_xy.c
@@ -4,24 +4,31 @@
 int main() {
     // Declaration of File pointer
     FILE *binfile;
-    double number;
+
+    // Initialize memory allocation for numbers
+    float *numbers;
+    int nums = 0;
+    numbers = (float*)malloc(sizeof(float) * nums);
 
     // Open file as read-only
     binfile = fopen("function_data_bin", "r");
-
+    
     // Validate file pointer
     if (binfile != NULL) {
         // Process the data
         do {
+            numbers = (float*)realloc(numbers, sizeof(float) * nums + 1);
 
-            fread(&number, sizeof(double), 1, binfile);
-            printf("%f\n", number);
+            fread(&numbers[nums], sizeof(float), 1, binfile);
+            printf("%f\n", numbers[nums]);
+            nums++;
 
         } while ( !feof(binfile) );
     }
-
+    
+    free(numbers);
     fclose(binfile); // Close the file.
-    printf("OK.\n");
+    printf("OK. %d\n", nums);
 
     return 1;
 }
