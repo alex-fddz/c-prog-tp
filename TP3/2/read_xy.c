53f5448758b169c7f9eca63480b2b35ce7b3900f
Alex Fernandez <javier.fddz@gmail.com>
2022-05-14 11:00:59 +0200

    Write out X Y pairs to txt file

--- TP3/2/read_xy.c
+++ TP3/2/read_xy.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    // Declaration of File pointer
-    FILE *binfile;
+int parseBinToTxt() {
+    // Declaration of File pointers
+    FILE *binfile; // Binary data file (read)
+    FILE *txtfile; // Text data file (write)
 
     // Initialize memory allocation for numbers
-    float *numbers;
-    int nums = 0;
+    float *numbers;     // Array to store numbers
+    int nums = 0;       // Total number of numbers
+    int n = 0;          // Number of x,y pairs
+    float x = 0, y = 0; // Values for x,y pair
     numbers = (float*)malloc(sizeof(float) * nums);
 
-    // Open file as read-only
+    // Open binary file as read-only
     binfile = fopen("function_data_bin", "r");
-    
-    // Validate file pointer
-    if (binfile != NULL) {
-        // Process the data
-        do {
+    if (binfile != NULL) { 
+        do { // Read data & save numbers to array
             numbers = (float*)realloc(numbers, sizeof(float) * nums + 1);
-
             fread(&numbers[nums], sizeof(float), 1, binfile);
-            printf("%f\n", numbers[nums]);
             nums++;
-
-        } while ( !feof(binfile) );
+            // printf("%f\n", numbers[nums]);
+        } while ( !feof(binfile) ); nums--; // There is 1 extra loop... ?
     }
+    fclose(binfile); // Close the -read- file.
     
+    // Open file for writing
+    txtfile = fopen("function_data", "w");
+    if (txtfile != NULL) { // Process numbers for x,y pairs
+        n = nums / 2;
+        for (int i = 0; i < n; i++) {
+            x = numbers[i];
+            y = numbers[n+i];
+            // Write out to text file
+            fprintf(txtfile, "%f %f\n", x, y);
+        }
+    }
+    fclose(txtfile); // Close the -write- file. 
+
     free(numbers);
-    fclose(binfile); // Close the file.
-    printf("OK. %d\n", nums);
 
     return 1;
 }
+
+
